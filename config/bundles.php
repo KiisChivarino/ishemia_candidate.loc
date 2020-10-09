f985df0011d4827df94ffb9f493f2e71fdf2e11c
mlobanov <mlobanov@kvokka.com>
2020-10-09 10:39:56 +0300

    remove vich uploader

--- config/bundles.php
+++ config/bundles.php
@@ -15,5 +15,4 @@ return [
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     Tetranz\Select2EntityBundle\TetranzSelect2EntityBundle::class => ['all' => true],
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
 ];
