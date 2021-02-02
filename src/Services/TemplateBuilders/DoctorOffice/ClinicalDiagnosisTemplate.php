c34ca4363e1d90fc52dad82436f98bb0a036e496
Vsevolod Lapshin <lpvs@kvokka.com>
2021-02-02 17:27:25 +0300

    Вроде протестил. Исправил ошибки.

---
+++ src/Services/TemplateBuilders/DoctorOffice/ClinicalDiagnosisTemplate.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services\TemplateBuilders\DoctorOffice;
+
+use App\Services\TemplateBuilders\AppTemplateBuilder;
+use App\Services\TemplateItems\FormTemplateItem;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class MedicalHistoryTemplate
+ *
+ * @package App\Services\TemplateBuilders\PatientOffice
+ */
+class ClinicalDiagnosisTemplate extends DoctorOfficeTemplateBuilder
+{
+    /** @var string[] Common form content for edit templates */
+    protected const EDIT_CONTENT = [
+        'anamnestic_h2' => 'Редактирование клинического диагноза',
+        'anamnestic_title' => 'Редактирование клинического диагноза',
+    ];
+
+    /** @var string[] Common ENTITY_CONTENT */
+    public const ENTITY_CONTENT = [
+        'entity' => \App\Services\TemplateBuilders\Admin\MedicalHistoryTemplate::ENTITY_CONTENT['entity'],
+    ];
+
+
+    /**
+     * MedicalHistoryTemplate constructor.
+     *
+     * @param RouteCollection $routeCollection
+     * @param string $className
+     */
+    public function __construct(RouteCollection $routeCollection, string $className)
+    {
+        parent::__construct($routeCollection, $className);
+        $this->addContent(
+            null,
+            null,
+            null,
+            self::EDIT_CONTENT,
+            null,
+            null,
+            null,
+            null,
+            self::ENTITY_CONTENT
+        );
+    }
+
+    /**
+     * @param object|null $entity
+     *
+     * @return AppTemplateBuilder
+     */
+    public function edit(?object $entity = null): AppTemplateBuilder
+    {
+        parent::edit();
+        $this->setCommonTemplatePath(($this->getTemplatePath()));
+        $this
+            ->getItem(FormTemplateItem::TEMPLATE_ITEM_FORM_NAME)
+            ->setPath($this->getTemplatePath())
+            ->addContentArray(
+                array_merge(
+                    \App\Services\TemplateBuilders\Admin\MedicalHistoryTemplate::FORM_SHOW_CONTENT,
+                    \App\Services\TemplateBuilders\Admin\MedicalHistoryTemplate::COMMON_CONTENT
+                )
+            );
+        return $this;
+    }
+}
