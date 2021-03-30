4cd374703d0f461b5690ba5f1f469da51b19f1c6
Vsevolod <lpvs@kvokka.com>
2021-03-30 09:22:17 +0000

    Apply suggestion to src/Controller/DoctorOffice/EditPatientTestingController.php

--- src/Controller/DoctorOffice/EditPatientTestingController.php
+++ src/Controller/DoctorOffice/EditPatientTestingController.php
@@ -132,7 +132,7 @@ class EditPatientTestingController extends DoctorOfficeAbstractController
      *     "/patient/{id}/patient_testing_overdue/{patientTesting}/edit",
      *     name="doctor_edit_patient_testing_from_overdue",
      *     methods={"GET","POST"},
-     *     requirements={"patient"="\d+", "patientTesting"="\d+"}
+     *     requirements={"id"="\d+", "patientTesting"="\d+"}
      *     )
      * @param Request $request
      * @param PatientTesting $patientTesting
