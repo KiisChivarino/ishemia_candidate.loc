ba0d372cfe424266eba3039e0b5d5cd9ea42552d
iigornnikiforov <iigornnikiforov@gmail.com>
2021-04-22 17:15:43 +0300

    Перевести на русский сообщение No results found в Select2EntityType

--- assets/js/select2.js
+++ assets/js/select2.js
@@ -1,3 +1,52 @@
 $(document).ready(function () {
     $().select2($('#data-values').data('init-json-class'));
+
+    /*
+        ENG: Russian translation of the message in Select2EntityType
+        РУС: Перевод на русский сообщения в Select2EntityType
+     */
+    $.fn.select2.amd.define('select2/i18n/ru',[],function () {
+        // Russian
+        return {
+            errorLoading: function () {
+                return 'Результат не может быть загружен.';
+            },
+            inputTooLong: function (args) {
+                let overChars = args.input.length - args.maximum;
+                let message = 'Пожалуйста, удалите ' + overChars + ' символ';
+                if (overChars >= 2 && overChars <= 4) {
+                    message += 'а';
+                } else if (overChars >= 5) {
+                    message += 'ов';
+                }
+                return message;
+            },
+            inputTooShort: function (args) {
+                let remainingChars = args.minimum - args.input.length;
+
+                return 'Пожалуйста, введите ' + remainingChars + ' или более символов';
+            },
+            loadingMore: function () {
+                return 'Загружаем ещё ресурсы…';
+            },
+            maximumSelected: function (args) {
+                let message = 'Вы можете выбрать ' + args.maximum + ' элемент';
+
+                if (args.maximum  >= 2 && args.maximum <= 4) {
+                    message += 'а';
+                } else if (args.maximum >= 5) {
+                    message += 'ов';
+                }
+
+                return message;
+            },
+            noResults: function () {
+                return 'Ничего не найдено';
+            },
+            searching: function () {
+                return 'Поиск…';
+            }
+        };
+    });
+
 });
